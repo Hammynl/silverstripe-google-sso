3e2fff9553a08a41bc13ec71b2565abebd7c497c
lars <lars.teeffelen@gmail.com>
2024-09-24 23:05:26 +0200

    Create and clean up google providers

--- src/Service/AdminManagementService.php
+++ src/Service/AdminManagementService.php
@@ -2,6 +2,7 @@
 
 namespace Larsvanteeffelen\SilverStripeGoogleSSO\Service;
 
+use Larsvanteeffelen\SilverStripeGoogleSSO\Model\GoogleProvider;
 use SilverStripe\Core\Injector\Injector;
 use SilverStripe\Security\Group;
 use SilverStripe\Security\IdentityStore;
@@ -12,7 +13,39 @@ class AdminManagementService {
 
     public function createOrLoginSsoUser(array $googleUserData): void
     {
+        $googleProvider = GoogleProvider::get()->filter([
+            'Sub' => $googleUserData['sub'],
+            'Email' => $googleUserData['email'],
+        ])->first();
 
+        // If Member connected to provider does not exist, delete & clear provider
+        if($googleProvider && $googleProvider->Member()->ID === 0) {
+            $googleProvider->delete();
+            $googleProvider = null;
+        }
+
+        // Create provider if non existent
+        if(!$googleProvider) {
+            $googleProvider = GoogleProvider::create();
+            $googleProvider->Sub = $googleUserData['sub'];
+            $googleProvider->Email = $googleUserData['email'];
+
+            // Get or create member
+            $member = Member::get()->filter(['Email' => $googleUserData['email']])->first();
+            if(!$member) {
+                $member = Member::create();
+                $member->FirstName = $googleUserData['given_name'];
+                $member->Surname = $googleUserData['family_name'];
+                $member->Email = $googleUserData['email'];
+                $member->generateRandomPassword(128);
+                $member->write();
+            }
+
+            $googleProvider->MemberID = $member->ID;
+            $googleProvider->write();
+        }
+
+        // Create group for Google SSO
         $ssoGroup = Group::get()->filter('Code', 'google-administrators')->first();
         if (!$ssoGroup) {
             $ssoGroup = Group::create();
@@ -23,28 +56,13 @@ class AdminManagementService {
             Permission::grant($ssoGroup->ID, 'ADMIN');
         }
 
-        $existingMember = Member::get()->filter('Email', $googleUserData['email'])->first();
-        if($existingMember) {
-            $existingMember->Sub = $googleUserData['sub'];
-            $existingMember->write();
-        }
-
-        $member = Member::get()->filter('sub', $googleUserData['sub'])->first();
-        if (!$member) {
-            $member = Member::create();
-            $member->Sub = $googleUserData['sub'];
-            $member->PictureUrl = $googleUserData['picture'];
-            $member->FirstName = $googleUserData['given_name'];
-            $member->Surname = $googleUserData['family_name'];
-            $member->Email = $googleUserData['email'];
-            $member->generateRandomPassword(128);
-            $member->write();
-        }
-
+        // Add member to group
+        $member = $googleProvider->Member();
         if (!$member->inGroup($ssoGroup)) {
             $ssoGroup->Members()->add($member);
         }
 
+        // Programmatically log in user
         Injector::inst()->get(IdentityStore::class)->logIn($member, true);
     }
 }
